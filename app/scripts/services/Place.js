bb524c5bbd0a20b686a122e40e77ca9353ecd1db
Steren <steren.giannini@gmail.com>
2013-10-20 18:11:56 +0200

    cleanup: removed unused extend() method

--- app/scripts/services/Place.js
+++ app/scripts/services/Place.js
@@ -3,12 +3,8 @@
 angular.module('faiteslemurApp')
   .factory('Place', function (Parse) {
 
-    //
-    var __hasProp = {}.hasOwnProperty;
-    var __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
-
     var Place = angular.copy(Parse.Model);
-    Place.configure('Place', 'name');
+    Place.configure('Place', 'name', 'objectId');
 
     return Place;
 
