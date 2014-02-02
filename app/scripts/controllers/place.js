a5a9f73ac8b9fcd0c3d1b3c942783f55408a148e
Steren <steren.giannini@gmail.com>
2014-02-02 23:15:35 +0100

    quick climb in place route list

--- app/scripts/controllers/place.js
+++ app/scripts/controllers/place.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('faiteslemurApp')
-  .controller('PlaceCtrl', function ($scope, $routeParams, $place, Route, Climb) {
+  .controller('PlaceCtrl', function ($scope, $place, Route, Climb) {
     $scope.place = $place;
 
     $scope.routes = Route.query({
@@ -13,5 +13,16 @@ angular.module('faiteslemurApp')
         }
       }
     });
+
+    $scope.quickClimb = function(routeId) {
+      var newClimb = new Climb();
+      newClimb.status = 'done';
+      Route.find(routeId).then(function(route) {
+        newClimb.route = route;
+        newClimb.save().then(function() {
+          console.log('climb saved');
+        });
+      });
+    };
 
   });
